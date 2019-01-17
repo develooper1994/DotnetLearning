864f46b05d6c79f8b7a373eb7d361f5ae70dd701
Mustafa Selcuk Caglar <selcukcaglar08@gmail.com>
2019-01-17 18:03:04 +0300

    17/01/2019 - 18:03

--- ExerciseWpf/MainWindow.xaml.cs
+++ ExerciseWpf/MainWindow.xaml.cs
@@ -24,5 +24,62 @@ namespace ExerciseWpf
         {
             InitializeComponent();
         }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            var desc = DescriptionTB.Text;
+            var mesg = string.Format($"The description is: {desc}");
+            if (string.IsNullOrEmpty(desc))
+            {
+                mesg = string.Format("Description is empty. Please fill the description.");
+            }
+            MessageBox.Show(mesg);
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            WeldCB.IsChecked = AssemblyCB.IsChecked = PlasmaCB.IsChecked = LaserCB.IsChecked = PurchaseCB.IsChecked =
+                LatheCB.IsChecked = DrillCB.IsChecked = FoldCB.IsChecked = RollCB.IsChecked = SawCB.IsChecked = false;
+
+            LenghtTB.Text = string.Empty;
+            NoteTxt.Text = string.Empty;
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CB_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = sender as CheckBox;
+
+            StringBuilder builtString = new StringBuilder(); // Much more efficient. Get used to it. Not always efficient
+            builtString.Append(cb.Content as string);
+
+            LenghtTB.Text += builtString.ToString();
+        }
+
+        private void Finish_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NoteTxt is null)
+                return;
+
+            ComboBox cb = sender as ComboBox;
+            ComboBoxItem cbItem = cb.SelectedItem as ComboBoxItem;
+            string item = cbItem.Content as string;
+
+            NoteTxt.Text = item;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Finish_SelectionChanged(FinishDropDown, null);
+        }
+
+        private void SupplierNameTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MassTB.Text = SupplierNameTB.Text;
+        }
     }
 }
